bd93c3f2f5d8cc5392cc381b67c616eb25f6ce6f
Nathan Che <nanache21@gmail.com>
2026-03-06 16:29:35 -0800

    initial

--- strand/demo/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import strandPreset from '../tailwind-preset.js'
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  presets: [strandPreset],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
-}
