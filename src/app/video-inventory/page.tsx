11730e1b8b4f288fbb223422df51d418bf4d7485
Nathan Che <nanache21@gmail.com>
2026-05-15 09:06:39 -0700

    Updated ignore  list

--- src/app/video-inventory/page.tsx
+++ src/app/video-inventory/page.tsx
@@ -64,7 +64,8 @@ export default function VideoInventoryPage() {
         "69ba190b3571b38304a680b0",
         "69ba19712fc4a03916fd0d7b",
         "69ba19ddbd3198ff9f5ae83b",
-        "6a035a24aa1ea09f190cc121"
+        "6a035a24aa1ea09f190cc121",
+        '6a07434e77e1b76830913d87'
     ]
 
     // Split ready vs. still-indexing videos
